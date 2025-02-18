21d5d9696d65dfb504ff0c459eee7eb9f4c7dcdd
Hikaru Tomihama <agpamwdjt1234@gmail.com>
2025-02-18 10:34:33 +0000

    corsの追加

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,7 +1,6 @@
 import sqlite3 from 'sqlite3'
 import { Database, open } from 'sqlite'
 
-// データベース接続をPromiseで返す関数
 export async function getDb(): Promise<Database> {
   return open({
     filename: './database.sqlite',
