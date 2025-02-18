879249d3f448dbf621bc4fac36e460e59ec0dd5e
038bkn <038bkn@gmail.com>
2025-02-19 02:38:28 +0900

    ルーティング設定を追加

---
+++ client/src/pages/Home.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return <h1>Home（トップ画面）</h1>;
+  };
+  
+  export default Home;
+  
---
+++ client/src/pages/Login.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return <h1>Home（ログイン画面）</h1>;
+  };
+  
+  export default Home;
+  
