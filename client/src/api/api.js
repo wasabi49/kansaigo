4dbf0ec23bba3fe62fc0b209781b09b3cc3a2e22
038bkn <038bkn@gmail.com>
2025-02-23 04:56:50 +0900

    feat: APIクライアントを作成し、ログインおよびダイアクト取得機能を追加

---
+++ client/src/api/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080"; // バックエンドのURL
+
+export const api = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true, // セッション情報を送るため必須
+});
+
+export const login = async (email, password) => {
+    return api.post("/auth/local/login", { mail_address: email, password });
+};
+
+export const fetchDialects = async () => {
+    return api.get("/dialects"); // 認証情報を送るため、`api.js` 内の `withCredentials: true` を利用
+};
+
+export const fetchQuestsByDialect = async (dialectId) => {
+    return api.get(`/dialects/${dialectId}/quests`);
+};
