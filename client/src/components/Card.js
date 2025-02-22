f22f2f07982f34ef960a671240de515d1cb69327
038bkn <038bkn@gmail.com>
2025-02-22 16:16:54 +0900

    feat: CardコンポーネントのonClickプロパティを削除し、スタイルを調整

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,16 +1,16 @@
 import { Box, Text } from "@yamada-ui/react";
 
-const Card = ({ title, onClick }) => {
+const Card = ({ title }) => {
   return (
-    <Box
-      p="4"
+    <Box 
       bg="white"
       borderRadius="lg"
       boxShadow="md"
-      _hover={{ bg: "gray.100", cursor: "pointer" }}
-      onClick={onClick}
-      w="full"
+      p="4"
+      width="100%" 
+      maxWidth={{ base: "600px", md: "90%" }} // PC: 600px, スマホ: 90%
       textAlign="center"
+      mx="auto"
     >
       <Text fontSize="lg" fontWeight="bold">{title}</Text>
     </Box>
