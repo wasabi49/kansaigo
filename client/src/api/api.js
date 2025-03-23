e27b26b93cae94c2721fb131bb221874462e8225
Hikaru Tomihama <agpamwdjt1234@gmail.com>
2025-03-23 22:32:23 +0900

    feat: バックエンドURLを環境変数に変更し、ビルドファイルを更新
    - BACKEND_URLを環境変数から取得するようにapi.jsを修正
    - asset-manifest.json内のmain.jsのハッシュを更新
    - 不要なLICENSE.txtファイルを削除
    - index.html内のmain.jsのハッシュを更新

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080"; // バックエンドのURL
+const BASE_URL = process.env.REACT_APP_BACKEND_URL; // バックエンドのURL
 
 export const api = axios.create({
     baseURL: BASE_URL,
